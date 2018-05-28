ccade85b188a71514e629aa84a791e3d7ea795a4
pulkit <info.programmersstudio@gmail.com>
2018-05-28 17:25:06 +0530

    v-0.0.1 adding files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Project from './Components/Project';
+import Clients from './Components/Clients';
+import Blog from './Components/Blog';
+import Contact from './Components/Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+          <div className="App">
+            
+            <div className="Header">
+              <ul>
+                <li className="Header-logo"><Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}> PROGRAMMERS <span style={{color: 'rgb(103,202,218)'}}>STUDIO</span> </Link></li>
+                <li className="Header-content hover"><Link to={'/About'} style={{ textDecoration: 'none', color: 'white' }}> ABOUT </Link></li>
+                <li className="Header-content hover"><Link to={'/Services'} style={{ textDecoration: 'none', color: 'white' }}> SERVICES </Link></li>
+                <li className="Header-content hover"><Link to={'/Project'} style={{ textDecoration: 'none', color: 'white' }}> PROJECT </Link></li>
+                <li className="Header-content hover"><Link to={'/Clients'} style={{ textDecoration: 'none', color: 'white' }}> CLIENTS </Link></li>
+                <li className="Header-content hover"><Link to={'/Blog'} style={{ textDecoration: 'none', color: 'white' }}> BLOG </Link></li>
+                <li className="Header-content hover"><Link to={'/Contact'} style={{ textDecoration: 'none', color: 'white' }}> CONTACT </Link></li>
+              </ul>
+            </div>
+            
+            {/* <div className="Space"></div> */}
+
+            <div className="App-Body">
+              <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/About' component={About} />
+                <Route exact path='/Services' component={Services} />
+                <Route exact path='/Project' component={Project} />
+                <Route exact path='/Clients' component={Clients} />
+                <Route exact path='/Blog' component={Blog} />
+                <Route exact path='/Contact' component={Contact} />
+              </Switch>
+            </div>
+          
+          </div>  
+        </Router>
     );
   }
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './src/Home.css';
+
+export default class Home extends Component {
+    render(){
+        return(
+            <div>
+                <div id="parallax" className="parallax"></div>
+                
+                <div id="info-part" className="info-part">
+                    <div id="box1" className="box">
+                        <h1>Services</h1>
+                        <h4>Checkout our services here.</h4>
+                        <button id="services-more" >More Info.</button>
+                    </div>
+                    <div id="box2" className="box">
+                        <h1>Projects</h1>
+                        <h4>Have a look at our work.</h4>
+                        <button id="h4rojects-more" >More Info.</button>
+                    </div>
+                    <div id="box3" className="box">
+                        <h1>Clients</h1>
+                        <h4>Our trusted clients.</h4>
+                        <button id="clients-more" >More Info.</button>
+                    </div>
+                </div>
+
+                <div id="myAbout" className="about-us">
+                    <div className="about-child">
+                        <h1>ABOUT US</h1>
+                        <p id="read-more-normal"><b>Programmers Studio</b> was founded by Mr. Pulkit Khullar in the year 2018, with the sole motive of changing the world with technology.
+                            The idea of flying cars and speaking robots in the future has been on the minds of people in this world. And here, at Programmers Studio, through hard work and determination, we strive to achieve that.
+                            Here, at this company, we believe that, “The best way to predict the future is to invent it. ”, as rightly said by Alan Kay.
+                        </p>
+                        <button id="read-more">Read More</button>
+                    </div>
+                </div>
+
+                <div id="myContact" className="contact-us">
+                    <div className="contact-child">
+                        <h1>CONTACT US</h1>
+                        <p>
+                            C-572A, Street-12, Majlis Park, Adarsh Nagar, Delhi - 110033.<br/>
+                            Nearest Metro - Adarsh Nagar<br/>
+                            Email Us : info.programmersstudio@gmail.com<br/>
+                            Call Us : +91 9650684491
+                        </p>
+                        <form className="form-group">
+                            <input className="form-control" type="text" placeholder="Name" />
+                            <input className="form-control" type="email" placeholder="Email" />
+                            <input className="form-control" type="text" placeholder="Subject" />
+                            <input className="form-control" type="text" placeholder="Message" />
+                            <button className="form-control" id="form-btn">Submit</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
